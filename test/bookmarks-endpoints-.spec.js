64ccb16c71a53eb36b22293ccc617ffb852dd735
Danielle Marie <dhutchings36@yahoo.com>
2019-11-07 17:46:10 -0600

    fixed bookmarks test

--- test/bookmarks-endpoints-.spec.js
+++ test/bookmarks-endpoints-.spec.js
@@ -6,10 +6,12 @@ describe('Bookmarks Endpoints', () => {
   let db
 
   before('make knex instance', () => {
+    
     db = knex({
       client: 'pg',
       connection: process.env.TEST_DB_URL,
     })
+    console.log(process.env.TEST_DB_URL)
     app.set('db', db)
   })
 
